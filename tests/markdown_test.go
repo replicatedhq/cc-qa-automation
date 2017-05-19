c47ee7305c3303ed39045ef4953a4a15660c00fc
wlaoh <winston@replicated.com>
2017-05-19 12:12:14 -0700

    cleaning up

--- tests/markdown_test.go
+++ tests/markdown_test.go
@@ -9,18 +9,16 @@ var _ = Describe("Markdown API", func() {
 
 	Describe("Given a working Github API", func() {
 
-		BeforeEach(func() {
-			// TODO: INSERT CODE HERE TO VERIFY THAT THE GITHUB IS UP AND RUNNING
-		})
+		// TODO: INSERT CODE HERE TO VERIFY THAT THE GITHUB IS UP AND RUNNING
 
-		Context("When a call is made to the markdown endpoint", func() {
+		Context("When a call is made to the markdown endpoint {TODO: ADD IN SPECIFIC TEST ex. with incorrect markdown}", func() {
 
-			BeforeEach(func() {
-				// TODO: INSERT CODE HERE TO SEND A REQUEST TO THE MARKDOWN ENDPOINT
-				// https://developer.github.com/v3/markdown/
-			})
+			// TODO: INSERT CODE HERE TO SEND A REQUEST TO THE MARKDOWN ENDPOINT
+			// https://developer.github.com/v3/markdown/
 
 			Specify("Then the markdown endpoint will return the correct output", func() {
+
+				// TODO: INSERT CODE HERE TO VERIFY QUALITY
 
 			})
 		})
@@ -29,41 +27,30 @@ var _ = Describe("Markdown API", func() {
 	// TODO: Add more scenarios against the markdown API, feel free to keep adding more by uncommenting the below
 	// and change the scenario descriptions to whatever you feel would be a useful test:
 
-	// Describe("Given a working Github API", func() {
+	// 	Context("When a call is made to the markdown endpoint {TODO: ADD IN SPECIFIC TEST ex. with incorrect markdown}", func() {
 
-	// 	BeforeEach(func() {
-	// 		// TODO: INSERT CODE HERE TO VERIFY THAT THE GITHUB IS UP AND RUNNING
-	// 	})
-
-	// 	Context("When a call is made to the markdown endpoint", func() {
-
-	// 		BeforeEach(func() {
-	// 			// TODO: INSERT CODE HERE TO SEND A REQUEST TO THE MARKDOWN ENDPOING
-	// 			// https://developer.github.com/v3/markdown/
-	// 		})
+	// 		// TODO: INSERT CODE HERE TO SEND A REQUEST TO THE MARKDOWN ENDPOING
+	// 		// https://developer.github.com/v3/markdown/
 
 	// 		Specify("Then the markdown endpoint will return the correct output", func() {
+
+	// TODO: INSERT CODE HERE TO VERIFY QUALITY
 
 	// 		})
 	// 	})
 	// })
 
-	// Describe("Given a working Github API", func() {
+	// 	Context("When a call is made to the markdown endpoint {TODO: ADD IN SPECIFIC TEST ex. with incorrect markdown}", func() {
 
-	// 	BeforeEach(func() {
-	// 		// TODO: INSERT CODE HERE TO VERIFY THAT THE GITHUB IS UP AND RUNNING
-	// 	})
-
-	// 	Context("When a call is made to the markdown endpoint", func() {
-
-	// 		BeforeEach(func() {
-	// 			// TODO: INSERT CODE HERE TO SEND A REQUEST TO THE MARKDOWN ENDPOING
-	// 			// https://developer.github.com/v3/markdown/
-	// 		})
+	// 		// TODO: INSERT CODE HERE TO SEND A REQUEST TO THE MARKDOWN ENDPOING
+	// 		// https://developer.github.com/v3/markdown/
 
 	// 		Specify("Then the markdown endpoint will return the correct output", func() {
+
+	// TODO: INSERT CODE HERE TO VERIFY QUALITY
 
 	// 		})
 	// 	})
 	// })
+
 })
--- tests/members_test.go
+++ tests/members_test.go
@@ -9,19 +9,17 @@ var _ = Describe("Members API", func() {
 
 	Describe("Given a working Github API", func() {
 
-		BeforeEach(func() {
-			// TODO: INSERT CODE HERE TO VERIFY THAT THE GITHUB IS UP AND RUNNING
-		})
+		// TODO: INSERT CODE HERE TO VERIFY THAT THE GITHUB IS UP AND RUNNING
 
-		Context("When a call is made to the check members endpoint checking if marccampbell is a member of replicatedcom", func() {
+		Context("When a call is made to the check members endpoint {TODO: ADD IN SPECIFIC TEST ex. marccampbell is a member of replicatedcom}", func() {
 
-			BeforeEach(func() {
-				// TODO: INSERT CODE HERE TO SEND A REQUEST TO THE CHECK MEMBERS ENDPOINT
-				// CHECKING IF `marccampbell` is a member of the organization `replicatedcom`
-				// https://developer.github.com/v3/orgs/members/
-			})
+			// TODO: INSERT CODE HERE TO SEND A REQUEST TO THE CHECK MEMBERS ENDPOINT
+			// Example: checking if `marccampbell` is a member of the organization `replicatedcom`
+			// https://developer.github.com/v3/orgs/members/
 
 			Specify("Then the members endpoint will verify if marccampbell is a member of replicatedcom", func() {
+
+				// TODO: INSERT CODE HERE TO VERIFY QUALITY
 
 			})
 		})
@@ -30,41 +28,29 @@ var _ = Describe("Members API", func() {
 	// TODO: Add more scenarios against the members API, feel free to keep adding more by uncommenting the below
 	// and change the scenario descriptions to whatever you feel would be a useful test:
 
-	// Describe("Given a working Github API", func() {
+	// 	Context("When a call is made to the check members endpoint {TODO: ADD IN SPECIFIC TEST ex. marccampbell is a member of replicatedcom}", func() {
 
-	// 	BeforeEach(func() {
-	// 		// TODO: INSERT CODE HERE TO VERIFY THAT THE GITHUB IS UP AND RUNNING
-	// 	})
-
-	// 	Context("When a call is made to the check members endpoint checking if marccampbell is a member of replicatedcom", func() {
-
-	// 		BeforeEach(func() {
 	// 			// TODO: INSERT CODE HERE TO SEND A REQUEST TO THE CHECK MEMBERS ENDPOINT
 	// 			// CHECKING IF `marccampbell` is a member of the organization `replicatedcom`
 	// 			// https://developer.github.com/v3/orgs/members/
-	// 		})
 
 	// 		Specify("Then the members endpoint will verify if marccampbell is a member of replicatedcom", func() {
+
+	// TODO: INSERT CODE HERE TO VERIFY QUALITY
 
 	// 		})
 	// 	})
 	// })
 
-	// Describe("Given a working Github API", func() {
+	// 	Context("When a call is made to the check members endpoint {TODO: ADD IN SPECIFIC TEST ex. marccampbell is a member of replicatedcom}", func() {
 
-	// 	BeforeEach(func() {
-	// 		// TODO: INSERT CODE HERE TO VERIFY THAT THE GITHUB IS UP AND RUNNING
-	// 	})
-
-	// 	Context("When a call is made to the check members endpoint checking if marccampbell is a member of replicatedcom", func() {
-
-	// 		BeforeEach(func() {
 	// 			// TODO: INSERT CODE HERE TO SEND A REQUEST TO THE CHECK MEMBERS ENDPOINT
 	// 			// CHECKING IF `marccampbell` is a member of the organization `replicatedcom`
 	// 			// https://developer.github.com/v3/orgs/members/
-	// 		})
 
 	// 		Specify("Then the members endpoint will verify if marccampbell is a member of replicatedcom", func() {
+
+	// TODO: INSERT CODE HERE TO VERIFY QUALITY
 
 	// 		})
 	// 	})
